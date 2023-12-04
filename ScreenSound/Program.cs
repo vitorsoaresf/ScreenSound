cc2a6ed35454d4df35af494f9f1b0fe10a743822
Vitor Soares <vitorsoaresf@gmail.com>
2023-12-04 14:53:57 -0300

    fix: change view properties from Musica class

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Musica musica1 = new Musica();
+musica1.nome = "minh_musica";
+musica1.artista = "Vitor";
+
+Console.WriteLine($"Nome da música {musica1.nome}");
+Console.WriteLine($"Artista da música {musica1.artista}");
+
+
+Musica musica2 =  new Musica();
+musica2.nome = "musica_Fidel";
+musica2.artista = "Fidel";
+
+Console.WriteLine($"Nome da música {musica2.nome}");
+Console.WriteLine($"Artista da música {musica2.artista}");
--- ScreenSound/obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound/obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fd88b4f57999e4e87683e6046c94886ead5da86")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
