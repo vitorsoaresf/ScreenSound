270722ef0347aed9b01e5da3e31133fea7ec13ce
Vitor Soares <vitorsoaresf@gmail.com>
2023-12-04 15:04:02 -0300

    feat: create method show music information

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -4,4 +4,20 @@
     public string artista;
     public int duracao;
     public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine($"Disponível ao plano");
+        }
+        else
+        {
+            Console.WriteLine($"Adquira o plano Plus+");
+        }
+        
+    }
 }
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,14 +1,13 @@
 ﻿Musica musica1 = new Musica();
 musica1.nome = "minh_musica";
 musica1.artista = "Vitor";
-
-Console.WriteLine($"Nome da música {musica1.nome}");
-Console.WriteLine($"Artista da música {musica1.artista}");
-
+musica1.duracao = 273;
+musica1.disponivel = true;
+musica1.ExibirFichaTecnica();
 
 Musica musica2 =  new Musica();
 musica2.nome = "musica_Fidel";
 musica2.artista = "Fidel";
-
-Console.WriteLine($"Nome da música {musica2.nome}");
-Console.WriteLine($"Artista da música {musica2.artista}");
+musica2.duracao = 367;
+musica2.disponivel = false;
+musica2.ExibirFichaTecnica();
