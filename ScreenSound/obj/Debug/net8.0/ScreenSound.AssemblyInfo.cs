7534e8fe8286fb947830aaba5f198f8a1110a16d
Vitor Soares <vitorsoaresf@gmail.com>
2023-12-05 14:28:11 -0300

    feat: implements genero class

--- ScreenSound/obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound/obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d040799c0f136e0902d2e15b0425ca595900bcd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
